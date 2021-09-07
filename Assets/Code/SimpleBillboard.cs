436dc6b3af31670cb317e571c6c085c1b8891d29
Tomasz Zając <tomezpl@gmail.com>
2021-09-07 17:20:32 +0100

    adds small offset to simplebillboard so that it moves to either x-axis side of parent depending on camera angle to trick z-ordering

--- Assets/Code/SimpleBillboard.cs
+++ Assets/Code/SimpleBillboard.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class SimpleBillboard : MonoBehaviour
 {
     Material billboardMaterial = null;
+    Vector3 initPos = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
         billboardMaterial = GetComponentInChildren<MeshRenderer>().material;
+        initPos = transform.localPosition;
     }
 
     // Update is called once per frame
@@ -24,6 +26,14 @@ public class SimpleBillboard : MonoBehaviour
             //transform.rotation = Quaternion.AngleAxis(-90f, Vector3.right) * Quaternion.AngleAxis(pitchDiff, Vector3.forward);
 
             transform.LookAt(currentCam.transform, Vector3.up);
+
+            if (transform.parent)
+            {
+                float xAxisDot = Vector3.Dot(transform.parent.right, (transform.position - currentCam.transform.position).normalized);
+                //transform.localPosition = initPos + new Vector3(xAxisDot * 3f, 0f);
+                Vector3 unitOffset = transform.worldToLocalMatrix.MultiplyVector(transform.right) * transform.localScale.magnitude;
+                transform.localPosition = initPos + unitOffset * 0.1f * -xAxisDot;
+            }
 
             UpdateShader();
             //transform.Rotate(transform.forward, 180f);
