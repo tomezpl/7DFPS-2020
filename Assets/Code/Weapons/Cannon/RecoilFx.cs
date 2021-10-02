791b6548eea0d0287d3d1a5cf168cfcd3b857106
Tomasz Zając <tomezpl@gmail.com>
2021-10-02 21:13:29 +0100

    comments

--- Assets/Code/Weapons/Cannon/RecoilFx.cs
+++ Assets/Code/Weapons/Cannon/RecoilFx.cs
@@ -7,17 +7,30 @@ using UnityEngine;
 /// </summary>
 public class RecoilFx : FxController
 {
+    /// <summary>
+    /// Axis to perform recoil rotation on. Usually X-axis as the recoil will kick the gun back.
+    /// </summary>
     public Vector3 PitchAxis = Vector3.right;
 
+    /// <summary>
+    /// Maximum amount of time a single recoil can take to reach <see cref="MaxRecoilPitch"/>.
+    /// </summary>
     public float MaxRecoilLerpTime = 0.06f;
 
+    /// <summary>
+    /// Peak amount of recoil kick (in degrees).
+    /// </summary>
     public float MaxRecoilPitch = -5f;
+
+    /// <summary>
+    /// Minimal amount of recoil kick (in degrees).
+    /// </summary>
     public float MinRecoilPitch = -3f;
 
     /// <summary>
     /// <para>
     /// For use in external scripts: 
-    /// this is the pitch quaternion that can be multiplied with the gun's input-based rotation.
+    /// this is the pitch <see cref="Quaternion"/> that can be multiplied with the gun's input-based rotation.
     /// </para>
     /// <para>
     /// Treat as this script's output value, needs to be applied in the right order because Quaternions.
@@ -25,6 +38,9 @@ public class RecoilFx : FxController
     /// </summary>
     public Quaternion RecoilPitchExternal = Quaternion.identity;
 
+    /// <summary>
+    /// Firing states to control animation behaviour.
+    /// </summary>
     enum FiringState
     {
         Idle = 0,
@@ -32,13 +48,24 @@ public class RecoilFx : FxController
         Firing
     }
 
+    /// <summary>
+    /// Current <see cref="FiringState"/> the weapon is in. Used to apply the right transformations.
+    /// </summary>
     FiringState currentFiringState = FiringState.Idle;
+
+    /// <summary>
+    /// Pre-defined target quaternions for each <see cref="FiringState"/>.
+    /// </summary>
     Quaternion[] targetQuaternions = new Quaternion[3];
 
+    /// <summary>
+    /// Time passed since start of current interpolation between target quaternions.
+    /// </summary>
     float elapsedTime = 0f;
 
     public override void Trigger()
     {
+        // Set current firing state as firing.
         currentFiringState = FiringState.Firing;
     }
 
@@ -52,11 +79,13 @@ public class RecoilFx : FxController
 
     void Update()
     {
+        // Reset firing state if maximum recoil has been reached.
         if(elapsedTime == MaxRecoilLerpTime)
         {
             currentFiringState = FiringState.Idle;
         }
 
+        // Apply the recoil transformations based on the firing state.
         RecoilPitchExternal = Quaternion.Slerp(Quaternion.identity, targetQuaternions[(int)currentFiringState], elapsedTime / MaxRecoilLerpTime);
 
         elapsedTime += Time.deltaTime * (currentFiringState == FiringState.Firing ? 1f : -1f);
