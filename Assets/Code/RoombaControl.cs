2f102f2fa75382b206da13509f8f04abe62d8122
Tomasz Zając <tomezpl@gmail.com>
2021-11-07 00:19:29 +0000

    fixes camera springarm spazzing out (ignores IgnoreRaycast)

--- Assets/Code/RoombaControl.cs
+++ Assets/Code/RoombaControl.cs
@@ -431,7 +431,7 @@ public class RoombaControl : NetworkBehaviour
 
         // Springarm camera: prevent objects from obstructing the player from the camera.
         float camRaycastHitDistance = cameraDistance;
-        RaycastHit[] raycastResults = Physics.RaycastAll(transform.position, Cam.transform.position - transform.position, cameraDistance + SpringarmCameraRaycastMargin, ~LayerMask.GetMask("LocalPlayer", "SmallProjectile"));
+        RaycastHit[] raycastResults = Physics.RaycastAll(transform.position, Cam.transform.position - transform.position, cameraDistance + SpringarmCameraRaycastMargin, ~LayerMask.GetMask("LocalPlayer", "SmallProjectile", "Ignore Raycast"));
         RaycastHit closestHit = default;
         for (int i = 0; i < raycastResults?.Length && i <= MaxCameraRaycastIterations; i++)
         {
