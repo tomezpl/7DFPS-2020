1d375f9c42f844f89d43dcfe7ca159f2c7491aea
Tomasz Zając <tomezpl@gmail.com>
2021-12-31 16:18:25 +0000

    adds hammer sfx

---
+++ Assets/Code/Audio/HammerAudioController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class HammerAudioController : MonoBehaviour
+{
+    public Knife Knife;
+    public AudioSource[] HitSoundEffects = new AudioSource[0];
+    public AudioSource HammerSwingAudioSrc;
+
+    bool wasSwingingLastFrame = false;
+    bool hitAPlayerLastFrame = false;
+
+    private void Start()
+    {
+        Knife ??= GetComponent<Knife>();
+
+        if (HammerSwingAudioSrc)
+        {
+            HammerSwingAudioSrc.spatialBlend = Knife.IsMine ? 0.25f : 1f;
+        }
+    }
+
+    private void Update()
+    {
+        if(HitSoundEffects?.Length > 0 && Knife.HitAPlayer.Value && !hitAPlayerLastFrame)
+        {
+            HitSoundEffects[Random.Range(0, HitSoundEffects.Length)].Play();
+        }
+
+        if (HammerSwingAudioSrc && Knife.IsSwinging && !wasSwingingLastFrame)
+        {
+            HammerSwingAudioSrc.Play();
+        }
+
+        wasSwingingLastFrame = Knife.IsSwinging;
+        hitAPlayerLastFrame = Knife.HitAPlayer.Value;
+    }
+}
--- Assets/Code/Weapons/Knife.cs
+++ Assets/Code/Weapons/Knife.cs
@@ -50,6 +50,10 @@ public class Knife : Weapon
     /// </summary>
     Quaternion initLocalOrientation;
 
+    public NetworkVariable<bool> HitAPlayer = new NetworkVariable<bool>(NetworkVariableReadPermission.Everyone, false);
+
+    public bool IsSwinging = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -121,6 +125,12 @@ public class Knife : Weapon
         {
             Hit = null;
             stabbedAlready = false;
+
+            if (IsMine)
+            {
+                IndicateHitServerRpc(false);
+            }
+
             return;
         }
 
@@ -130,6 +140,8 @@ public class Knife : Weapon
         // Is the stab lunge complete now (and we're recovering to idle position)?
         bool stabbed = stabAnimTimer < StabAnimDuration * .5f;
 
+        IsSwinging = !stabbed;
+
         // Interpolate knife position.
         //transform.localPosition = Vector3.Lerp(initLocalPosition, initLocalPosition + Vector3.forward * .33f, stabbed ? 1f - idleProgress : stabProgress);
 
@@ -138,6 +150,12 @@ public class Knife : Weapon
 
         // Update timer.
         stabAnimTimer -= Time.deltaTime;
+    }
+
+    [ServerRpc]
+    public void IndicateHitServerRpc(bool hitState, ServerRpcParams rpcParams = default)
+    {
+        HitAPlayer.Value = hitState;
     }
 
     private void OnTriggerStay(Collider other)
@@ -157,6 +175,8 @@ public class Knife : Weapon
 
             // Calculate the damage for this stab. A perfect backstab should deal maximum damage (DamagePerBackstab).
             int stabDamage = Mathf.RoundToInt(DamagePerBackstab * Mathf.Max(0f, Vector3.Dot(Owner.GetComponent<RoombaControl>().transform.forward, otherPlayer.transform.forward)));
+
+            IndicateHitServerRpc(true);
 
             Owner.GetComponent<RoombaControl>().DealDamageServerRpc(stabDamage, otherPlayer.OwnerClientId);
         }
