ade70c36cb2f78d9e7f920a614c6da1721f0e5a8
Tomasz Zając <tomezpl@gmail.com>
2022-05-05 19:58:00 +0100

    redoing ui with uitk

--- Assets/Code/MenuManager.cs
+++ Assets/Code/MenuManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -13,17 +15,74 @@ public class MenuManager : MonoBehaviour
 
     public string[] MapTitles = new string[1];
     public int[] MapIndices = new int[1];
+    public string[] MapDescriptions = new string[1];
+    public Sprite[] MapPreviews = new Sprite[1];
+
+    public Image MapPreview;
+    public TMPro.TMP_Text MapTitle;
+    public TMPro.TMP_Text MapDesc;
+
+    public GameObject MapSelectionScreen;
+    public TMPro.TMP_Dropdown MapDropdown;
+
+    public GameObject MultiplayerScreen;
+
+    public GameObject MainScreen;
 
     // Start is called before the first frame update
     void Start()
     {
         MapIndex = MapIndices[0];
+        MapDropdown.options = MapTitles.Select(title => new TMPro.TMP_Dropdown.OptionData(title)).ToList();
+        UpdateMapPreview();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ReadMapSelection()
+    {
+        MapIndex = MapIndices[MapDropdown.value];
+
+        UpdateMapPreview();
+    }
+
+    public void UpdateMapPreview()
+    {
+        MapPreview.sprite = MapPreviews[MapDropdown.value];
+        MapDesc.text = MapDescriptions[MapDropdown.value];
+        MapTitle.text = MapTitles[MapDropdown.value];
+    }
+
+    public void ShowMaps()
+    {
+        MapSelectionScreen.SetActive(true);
+        MultiplayerScreen.SetActive(true);
+
+        MainScreen.SetActive(false);
+    }
+
+    public void ShowMainScreen()
+    {
+        MapSelectionScreen.SetActive(false);
+        MultiplayerScreen.SetActive(false);
+
+        MainScreen.SetActive(true);
+    }
+
+    public void LoadMapAsHost()
+    {
+        IsHost = true;
+        LoadMap();
+    }
+
+    public void LoadMapAsClient()
+    {
+        IsHost = false;
+        LoadMap();
     }
 
     public void LoadMap()
